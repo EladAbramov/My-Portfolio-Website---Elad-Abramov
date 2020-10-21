89b1836ed072bdb572c663333d149d054826697a
eladabramov1989 <eladsoftware20@gmail.com>
2020-10-21 11:32:51 +0300

    make it better

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const passport = require('passport');
-const routes = require('./routes/index');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+
 const app = express()
+app.use('/', serveStatic(path.join(__dirname, '/dist')))
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
-}
-app.use(cors());
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(routes)
-app.use(passport.initialize())
-require('./config/passport')(passport)
+const port = process.env.PORT || 8080
+app.listen(port)
 
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
+console.log("Listening to port: " + port)
